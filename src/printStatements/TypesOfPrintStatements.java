72d509717b80f918875e6f645abdf46e918090be
anveshvyata <vyata.anvesh@gmail.com>
2024-11-07 23:07:00 -0600

    TypesOfPrintStatements example

---
+++ src/printStatements/TypesOfPrintStatements.java
@@ -0,0 +1,10 @@
+package printStatements;
+
+public class TypesOfPrintStatements {
+
+    public static void main (String args[]){
+        System.out.print("This is print");
+        System.out.println("This is println");
+        System.out.printf("This is printf generated on Day %d of %s Month",07,"November");
+    }
+}
